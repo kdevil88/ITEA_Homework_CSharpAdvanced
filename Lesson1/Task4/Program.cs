96ee2c5733d8e21d225a4988673661ce0257f26d
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2020-12-23 16:13:16 +0200

    Lesson 1

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            if (time == null)
+            {
+                /* Условие никогда не выполнится т.к. переменная типа DateTime не может принимать значение null */
+            }
+
+            DateTime? time2 = null;
+            if (time2 == null)
+            {
+               Console.WriteLine(time2.HasValue);
+                /* Применяя nullable тип - условие выполнится*/
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
