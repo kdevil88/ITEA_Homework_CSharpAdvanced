4deb32314c40cfa8ee476f356aeaaababa9c0a5d
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-03 16:56:22 +0200

    Lesson3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        delegate int SomeDelegate(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            SomeDelegate avg_calc = delegate(int val1, int val2, int val3) { return (val1 + val2 + val3) / 3; };
+            int res = avg_calc(5, 50, 500);
+            Console.WriteLine("{0} result is {1}", avg_calc, res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task2
+{
+    class Calculator2
+    {
+        public delegate double CalcAction(double num1, double num2);
+        public CalcAction Add = (n1, n2) => { return n1 + n2; };
+        public CalcAction Substract = (n1, n2) => { return n1 - n2; };
+        public CalcAction Multiply = (n1, n2) => { return n1 * n2; };
+        public CalcAction Divide = (n1, n2) => 
+            { 
+                try
+                {
+                    if (n2 == 0) 
+                        throw new DivideByZeroException();
+                    return n1 / n2;
+                }
+                catch (DivideByZeroException)
+                {
+                    return 0;
+                }
+            };
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator2 Calc = new Calculator2();
+            string operation;
+            double number1, number2;
+            while (true)
+            {
+                Console.Write("Enter first number: ");
+                number1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter operation (+,-,*,/): ");
+                operation = Console.ReadLine();
+                Console.Write("Enter second number: ");
+                number2 = double.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine("Answer is: {0}", Calc.Add(number1, number2));
+                        break;
+                    case "-":
+                        Console.WriteLine("Answer is: {0}", Calc.Substract(number1, number2));
+                        break;
+                    case "*":
+                        Console.WriteLine("Answer is: {0}", Calc.Multiply(number1, number2));
+                        break;
+                    case "/":
+                        Console.WriteLine("Answer is: {0}", Calc.Divide(number1, number2));
+                        break;
+                    default:
+                        Console.WriteLine("Operation not supported");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
