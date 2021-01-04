18b7ca4d49ed140901ac683180ce6c54a5c11677
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-04 21:27:12 +0200

    Lesson3

---
+++ Lesson3/Task4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ShowStopWatchElapsed()
+        {
+           teDisplay.Text = string.Format("{0}", sw.Elapsed);
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            sw.Start();
+            timer.Enabled = true;
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            timer.Enabled = false;
+            ShowStopWatchElapsed();
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            sw.Reset();
+            timer.Enabled = false;
+            ShowStopWatchElapsed();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ShowStopWatchElapsed();
+        }
+    }
+}
