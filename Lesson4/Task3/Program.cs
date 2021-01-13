1711d12f13b5e878875d6a852499cb45bf717070
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-13 20:39:53 +0200

    Lesson4

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            IEnumerable<int> RandomRange = Enumerable.Range(1, 30).Select(x => rand.Next(-100, 100)).ToList();
+
+            var positive = RandomRange.Where(x => x > 0);
+            Console.WriteLine("Первый положительный элемент: {0}", positive?.First());
+            var negative = RandomRange.Where(x => x < 0);
+            Console.WriteLine("Последний негативный элемент: {0}", negative?.Last());
+
+            Console.WriteLine(new string('-', 10));
+            foreach (int num in RandomRange)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine(new string('-', 10));
+
+            Console.ReadKey();
+        }
+    }
+}
