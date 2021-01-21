cb786edc934e855567729374ff7699d6e037fd00
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-21 08:57:02 +0200

    Lesson5

---
+++ Lesson5/Task4/Model1.cs
@@ -0,0 +1,53 @@
+﻿namespace Task4
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Model1 : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Task4.Model1" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
+        // в файле конфигурации приложения.
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Teachers> TeachersList { get; set; }
+        public virtual DbSet<Groups> GroupsList { get; set; }
+        public virtual DbSet<Courses> CoursesList { get; set; }
+        public virtual DbSet<Students> StudentsList { get; set; }
+    }
+
+    public class Teachers
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+    }
+    public class Groups
+    {
+        public int Id { get; set; }
+        public Courses Course { get; set; }
+        public Teachers Teacher { get; set; }
+        public Students Student { get; set; }
+    }
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Students
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Lesson5/Task4/Program.cs
+++ Lesson5/Task4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,66 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer(new DropCreateDatabaseAlways<Model1>());
+            using (Model1 db = new Model1())
+            {
+                try
+                {
+                    Teachers t1 = new Teachers() { Id = 1, FIO = "Alex Glembitskij", Age = 27};
+                    Teachers t2 = new Teachers() { Id = 2, FIO = "Elon Musk", Age = 49 };
+                    db.TeachersList.AddRange(new List<Teachers>() { t1, t2 });
+                    db.SaveChanges();
+
+                    Console.WriteLine("In our Academy we have {0} teachers:", db.TeachersList.Count());
+                    foreach (var item in db.TeachersList)
+                    {
+                        Console.WriteLine("{0}. {1}", item.Id, item.FIO);
+                    }
+
+                    Students s1 = new Students() { Id = 1, FIO = "Andrey Andreeev" };
+                    Students s2 = new Students() { Id = 2, FIO = "Viktor Federovich" };
+                    Students s3 = new Students() { Id = 3, FIO = "Petr Petrovich" };
+                    Students s4 = new Students() { Id = 4, FIO = "Vladimir Aleksandrovich" };
+                    db.StudentsList.AddRange(new List<Students>() { s1, s2, s3, s4 });
+                    db.SaveChanges();
+
+                    Console.WriteLine("\nbest students:");
+                    foreach (var item in db.StudentsList)
+                    {
+                        Console.WriteLine("{0}. {1}", item.Id, item.FIO);
+                    }
+
+                    Courses c1 = new Courses() { Id = 1, Name = "C# Advanced" };
+                    Courses c2 = new Courses() { Id = 2, Name = "How to create hyperloop" };
+                    db.CoursesList.AddRange(new List<Courses>() { c1, c2 });
+                    db.SaveChanges();
+
+                    Console.WriteLine("\nand interesting courses:");
+                    foreach (var item in db.CoursesList)
+                    {
+                        Console.WriteLine("{0}. {1}", item.Id, item.Name);
+                    }
+
+                    db.GroupsList.Add(new Groups() { Course = c1, Teacher = t1, Student = s1 });
+                    db.GroupsList.Add(new Groups() { Course = c1, Teacher = t1, Student = s2 });
+                    db.GroupsList.Add(new Groups() { Course = c1, Teacher = t1, Student = s3 });
+                    db.GroupsList.Add(new Groups() { Course = c2, Teacher = t2, Student = s4 });
+                    db.SaveChanges();
+
+                    Console.WriteLine(new string('-', 50));
+
+                    Console.WriteLine("Our groups be like:");
+                    foreach (var item in db.GroupsList)
+                    {
+                        Console.WriteLine("{0} - {1} - {2}", item.Course.Name, item.Teacher.FIO, item.Student.FIO);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
