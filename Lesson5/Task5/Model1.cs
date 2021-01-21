ce1fa7c9ec1c5d8ea40c6a22272a28cd393c79ea
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-21 09:50:38 +0200

    Lesson5

---
+++ Lesson5/Task5/Model1.cs
@@ -0,0 +1,38 @@
+﻿namespace Task5
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Model1 : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Task5.Model1" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
+        // в файле конфигурации приложения.
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Courses> CoursesList { get; set; }
+        public virtual DbSet<CourseDetail> CourseDetails { get; set; }
+    }
+
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class CourseDetail
+    {
+        public int Id { get; set; }
+        public Courses Course { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ Lesson5/Task5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<Model1>());
+            using (Model1 db = new Model1())
+            {
+                try
+                {
+                    Courses c1 = new Courses() { Id = 1, Name = "Python for dummies" };
+                    Courses c2 = new Courses() { Id = 2, Name = "C# advanced" };
+                    Courses c3 = new Courses() { Id = 3, Name = ".NET Core web development" };
+                    db.CoursesList.AddRange(new List<Courses>() { c1, c2, c3 });
+                    db.SaveChanges();
+
+                    // orderby, count
+                    Console.WriteLine("There a {0} courses:\n", db.CoursesList.Count());
+                    foreach (var item in db.CoursesList.OrderBy(n => n.Name))
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+                    Console.WriteLine(new string('-', 50));
+
+                    db.CourseDetails.Add(new CourseDetail() { Course = c1, Duration = 10 });
+                    db.CourseDetails.Add(new CourseDetail() { Course = c2, Duration = 12 });
+                    db.CourseDetails.Add(new CourseDetail() { Course = c3, Duration = 14 });
+                    db.SaveChanges();
+
+                    // first
+                    var first = db.CourseDetails.First(n => n.Course.Name.StartsWith("C#"));
+                    Console.WriteLine("First found C# course: {0}", first?.Course.Name);
+                    // firstordefault
+                    var firstordefault = db.CourseDetails.FirstOrDefault(n => n.Course.Name.StartsWith("Delphi"));
+                    Console.WriteLine("FirstOrDefault found Delphi course: {0}", 
+                        firstordefault == null ? "не найдено" : firstordefault.Course.Name);
+                    //min
+                    var min = db.CourseDetails.Min(d => d.Duration);
+                    Console.WriteLine("Shortest course duration: {0} hours", min);
+                    //max
+                    var max = db.CourseDetails.Max(d => d.Duration);
+                    Console.WriteLine("Longest course duration: {0} hours", max);
+                    //average
+                    var avg = db.CourseDetails.Average(d => d.Duration);
+                    Console.WriteLine("Average course duration: {0} hours", avg);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
