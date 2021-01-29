a4cfa687220e7cfdbc01f03eb6cdc40ca15e96ca
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-29 12:08:26 +0200

    Lesson8

---
+++ Lesson8/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task1
+{
+    class Program
+    {
+        static XmlDocument xmldoc;
+        static XmlElement CreateContact(string name, string phone)
+        {
+            XmlElement contact = xmldoc.CreateElement("Contact");
+            contact.InnerText = name;
+            contact.SetAttribute("TelephoneNumber", phone);
+            return contact;
+        }
+        static void Main(string[] args)
+        {
+            xmldoc = new XmlDocument();
+            XmlDeclaration xmlDeclaration = xmldoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmldoc.DocumentElement;
+            xmldoc.InsertBefore(xmlDeclaration, root);
+            XmlElement xmlroot = xmldoc.CreateElement("MyContacts");
+            xmlroot.AppendChild(CreateContact("Alex", "09977776523"));
+            xmlroot.AppendChild(CreateContact("Petr", "06832467236"));
+            xmlroot.AppendChild(CreateContact("Vova", "06398784358"));
+            xmldoc.AppendChild(xmlroot);
+            xmldoc.Save("TelephoneBook.xml");
+            Console.WriteLine("XML created with contents:\n");
+            Console.WriteLine(System.Xml.Linq.XDocument.Parse(xmldoc.InnerXml).ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson8/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = new FileStream("TelephoneBook.xml", FileMode.Open);
+            stream.Position = 0;
+            XmlTextReader xml = new XmlTextReader(stream);
+            Console.WriteLine("XML file info:\n");
+            while (xml.Read())
+            {
+                Console.WriteLine("{0,-15} {1,-15} {2,-15}",
+                    xml.NodeType,
+                    xml.Name, 
+                    xml.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
