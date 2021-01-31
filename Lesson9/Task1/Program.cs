c9f39f96d208718fcc6d5e65647453494b49b143
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-31 19:25:18 +0200

    Lesson9

---
+++ Lesson9/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class SomeClass
+    {
+        [ObsoleteAttribute("better call method B", false)]
+        public bool FieldA = true;
+        public bool FieldB = false;
+        [ObsoleteAttribute("better call method D", true)]
+        public bool FieldC = true;
+        public bool FieldD = false;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass some = new SomeClass();
+            bool b, d;
+            b = some.FieldA & some.FieldB;
+            Console.WriteLine("Result 1: {0}", b);
+            d = some.FieldC & some.FieldD;
+            Console.WriteLine("Result 2: {0}", d);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson9/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class AccessLevelAttribute : Attribute
+    {
+        public int level;
+        public AccessLevelAttribute(int level)
+        {
+            this.level = level;
+        }
+    }
+    class User
+    {
+
+    }
+    [AccessLevel(1)]
+    class Manager : User
+    {
+
+    }
+    [AccessLevel(2)]
+    class Programmer : User
+    {
+
+    }
+    [AccessLevel(3)]
+    class Director : User
+    {
+
+    }
+    class SecuredBase
+    {
+        public bool Access(User user)
+        {
+            bool result = false;
+            Type utype = user.GetType();
+            object[] attributes = utype.GetCustomAttributes(false);
+            foreach (var item in attributes)
+            {
+                if (item is AccessLevelAttribute)
+                    result = (item as AccessLevelAttribute).level > 2;
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecuredBase @base = new SecuredBase();
+            Console.WriteLine("Manager has access to secured base: {0}", @base.Access(new Manager()));
+            Console.WriteLine("Programmer has access to secured base: {0}", @base.Access(new Programmer()));
+            Console.WriteLine("Director has access to secured base: {0}", @base.Access(new Director()));
+            Console.ReadKey();
+        }
+    }
+}
