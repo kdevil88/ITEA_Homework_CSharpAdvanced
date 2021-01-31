6095ef211ebf26761f782cbc319a7e7929fffcf0
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-01-31 17:40:22 +0200

    Lesson8

--- Lesson8/Task4_Registry/Form1.cs
+++ Lesson8/Task4_Registry/Form1.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
