9b140ba27ec0a3389900126441792ffc73709d1d
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-02-07 13:53:38 +0200

    Lesson11

---
+++ Lesson11/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int method_num = 0;
+
+            RecursionMethod();
+
+            void RecursionMethod()
+            {
+                Console.WriteLine("Вызов метода в {0}-й раз, в потоке {1}", ++method_num, Thread.CurrentThread.GetHashCode());
+                Thread.Sleep(1000);
+                Thread recurs = new Thread(RecursionMethod);
+                recurs.Start();
+            }
+        }
+    }
+}
---
+++ Lesson11/Task2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    static class Matrix
+    {
+        static Random rand = new Random();
+        static object block = new object();
+
+        public static void Rain(object x)
+        {
+            int stringLength = rand.Next(5, Console.WindowHeight);
+            int X = (int)x; 
+            int Y = rand.Next(0, Console.WindowHeight);
+            int currY = 0; 
+            for (int i = 0; i < stringLength; i++)
+            {
+                lock (block)
+                {
+                    if (Y == Console.WindowHeight - 1)
+                    {
+                        Y = 0;
+                        Console.SetCursorPosition(X, Y);
+                        currY = Y + 1;
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(X, Y++);
+                        currY = Y + 1;
+                    } 
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("{0}", Convert.ToChar(rand.Next(100, 126)));
+                    if (currY > 3 && i >= 2)
+                    {
+                        Console.SetCursorPosition(X, currY - 3);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("{0}", Convert.ToChar(rand.Next(100, 126)));
+
+                        Console.SetCursorPosition(X, currY - 4);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("{0}", Convert.ToChar(rand.Next(100, 126)));
+                    }
+                    else if (currY <= 2)
+                    {
+                        Console.SetCursorPosition(X, currY - 4 + Console.WindowHeight);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("{0}", Convert.ToChar(rand.Next(100, 126)));
+
+                        Console.SetCursorPosition(X, currY - 5 + Console.WindowHeight);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("{0}", Convert.ToChar(rand.Next(100, 126)));
+                    }
+                    if (i == stringLength - 1)
+                    {
+                        if (Y >= stringLength)
+                        {
+                            Console.SetCursorPosition(X, Y - stringLength);
+                            Console.Write(' ');
+                            i--;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(X, Console.WindowHeight - stringLength + Y);
+                            Console.Write(' ');
+                            i--;
+                        }
+                    }
+                    Thread.Sleep(0);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            for (int MaxtrixWidth = 0; MaxtrixWidth < Console.WindowWidth - 1;)
+            {
+                new Thread(new ParameterizedThreadStart(Matrix.Rain)).Start(MaxtrixWidth);
+                MaxtrixWidth += 2;
+            }
+        }
+    }
+}
