7f29d519eaef4a3cf595297ea2fb8cffa124c2f4
kdevil88 <45238348+kdevil88@users.noreply.github.com>
2021-02-14 23:30:02 +0200

    work

---
+++ GraduationWork/GraduationWork/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraduationWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StoreContext db;
+        private User CurrentUser;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Database.SetInitializer(new DropCreateDatabaseAlways<StoreContext>());
+            db = new StoreContext();
+            db.Users.Load();
+            gridUsers.ItemsSource = db.Users.Local.ToBindingList();
+#if DEBUG
+            CurrentUser = db.Users.FirstOrDefault<User>(n => n.Name.Equals("admin"));
+#else
+            if (!TryLogin())
+                Application.Current.Shutdown();
+#endif
+        }
+
+        public enum Roles
+        {
+            Admin,
+            User
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool TryLogin()
+        {
+            bool login_result = false;
+            while (!login_result)
+            {
+                LoginWindow login = new LoginWindow();
+                if ((bool)login.ShowDialog())
+                {
+                    User login_user = db.Users.FirstOrDefault<User>(n => n.Name.Equals(login.txtLogin.Text.Trim()));
+                    login_result = login_user != null && login_user.Password.Equals(login.txtPassword.Password.Trim());
+                    if (!login_result)
+                    {
+                        MessageBox.Show("Неправильно указан логин или пароль");
+                        login.Close();
+                    }
+                    else
+                        CurrentUser = login_user;
+                }
+                else
+                    login_result = false;
+            }
+            return login_result;
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+        }
+
+        private void gridUsers_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*foreach (DataGridColumn column in gridUsers.ItemsSource)
+            {
+                switch (column.Header)
+                {
+                    case "Id":
+                        column.Visibility = Visibility.Hidden;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            */
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.SaveChanges();
+        }
+    }
+}
---
+++ GraduationWork/GraduationWork/StoreContext.cs
@@ -0,0 +1,72 @@
+﻿namespace GraduationWork
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class StoreContext : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "GraduationWork.Model1" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
+        // в файле конфигурации приложения.
+        public StoreContext()
+            : base("name=StoreContext")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Manager> Managers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+
+    public enum Roles
+    {
+        Admin,
+        User
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string FIO { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+    public class Manager
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string FIO { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public Product[] Product { get; set; }
+        public Customer Customer { get; set; }
+        public User User { get; set; }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+    }
+}
